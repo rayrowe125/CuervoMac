87acaeac5884c66b4624c56224a52b7353862f95
Ray Gonzalez <gonzai125@gmail.com>
2014-01-13 14:48:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CuervoMac::Application.config.secret_key_base = '8e4af803f58813737c481281b32f999ee46f8a3a57c85eae27cfd3beb96a72fa47432d8eda1ebf3340620862c56a2cab62c195716a04097ccad9b55b5081a814'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CuervoMac::Application.config.session_store :cookie_store, key: '_CuervoMac_session'
